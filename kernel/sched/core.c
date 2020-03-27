33fc1635dae3d2f2043964585c143c54bee1a961
PutraGandaD <putraganda.dewata@gmail.com>
2020-03-28 06:04:07 +0700

    Revert "kernel: Force trivial, unbound kthreads onto low-power CPUs by kerneltoast(SultanXDA)"
    This reverts commit ec57378650531e4c8e0d3d44bfd9c528f6f8418b.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7799,11 +7799,6 @@ int set_cpus_allowed_ptr(struct task_struct *p, const struct cpumask *new_mask)
 	if (p->flags & PF_PERF_CRITICAL)
 		new_mask = cpu_perf_mask;
 
-	/* Force all trivial, unbound kthreads onto the little cluster */
-	if (p->flags & PF_KTHREAD && p->pid != 1 &&
-		cpumask_equal(new_mask, cpu_all_mask))
-		new_mask = cpu_lp_mask;
-
 	rq = task_rq_lock(p, &flags);
 
 	if (cpumask_equal(&p->cpus_allowed, new_mask))
